1c30c498f7c7b1d9c545f8e277a785c432e2bafb
Jared Fleming <jaredmf10@gmail.com>
2020-01-23 19:07:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,10 @@ def summon_captain_planet(array)
   array.collect {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls(array)
-
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
+
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
